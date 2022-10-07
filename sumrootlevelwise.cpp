feefb6802dc85c548329cfd51762c5b322266a13
jayneeljariwala <51472658+jayneeljariwala@users.noreply.github.com>
2022-10-07 11:12:37 +0530

    Update sumrootlevelwise.cpp

--- sumrootlevelwise.cpp
+++ sumrootlevelwise.cpp
@@ -1,3 +1,77 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *right;
+    Node *left;
+    Node(){
+        right=NULL;
+        left=NULL;
+    }
+    Node(int n){
+        data=n;
+        right=NULL;
+        left=NULL;
+    }
+};
+
+Node *root=NULL;
+
+Node* createTree(Node *node=NULL){
+    int data;
+    cout << "Enter number (Enter -1 to stop creation) : ";
+    cin >> data;
+    if(data==-1){
+        return NULL;
+    }else{
+        cout << "Enter data at left node : ";
+        Node* tmp=new Node(data);
+        tmp->left=createTree();
+        cout << "Enter data at right node : ";
+        tmp->right=createTree();
+        return tmp;
+    }
+}
+
+void printLevelLeafSum(){
+    queue<pair<Node*,int>> qu;
+    Node *tmp;
+    int sum=0;
+    int lvl=1;
+    if(root==NULL){
+        cout << sum;
+    }else{
+        qu.push({root,lvl});
+        while(!qu.empty()){
+            tmp=qu.front().first;
+            if(lvl!=qu.front().second){
+                cout << sum << endl;
+                sum=0;
+            }
+            lvl=qu.front().second;
+            if(tmp->right==NULL && tmp->left==NULL){
+                sum+=tmp->data;
+            }else{
+                if(tmp->left!=NULL){
+                    qu.push({tmp->left,lvl+1});
+                }
+                if(tmp->right!=NULL){
+                    qu.push({tmp->right,lvl+1});
+                }
+            }
+            qu.pop();
+        }
+        cout << sum << endl;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    root=createTree();
+    cout << endl;
+    printLevelLeafSum();
+    cout << endl;
+    return 0;
+}
